b7afd67477c53a3dc3c7218f270f8c2969d81f99
Jay Patel <jay3686@gmail.com>
2016-04-04 01:11:14 -0400

    fixup and add more tests

--- server.js
+++ server.js
@@ -9,61 +9,59 @@ app.use(express.static('public'));
 var jsonParser = bodyParser.json();
 
 app.get('/items', function(req, res) {
-    res.json(storage.items);
+  res.json(storage.items);
 });
 
 app.post('/items', jsonParser, function(req, res) {
-    if (!req.body || !req.body.name) {
-        return res.status(400).json({error: 'Invalid item body'});
-    }
+  if (!req.body || !req.body.name) {
+    return res.status(400).json({error: 'Invalid item body'});
+  }
 
-    var item = storage.add(req.body.name);
+  var item = storage.add(req.body.name);
 
-    console.log(res.status(201).json);
-
-    res.status(201).json(item);
+  res.status(201).json(item);
 });
 
 app.delete('/items/:id', function(req, res) {
-    var itemId = parseInt(req.params.id);
-    if(isNaN(itemId)) {
-        return res.status(400).json({error: 'Invalid item id'});
-    }
+  var itemId = parseInt(req.params.id);
+  if(isNaN(itemId)) {
+    return res.status(400).json({error: 'Invalid item id'});
+  }
 
-    var matchedItem = storage.remove(itemId);
-    if (matchedItem){
-      return res.status(200).json(matchedItem);
-    }
+  var matchedItem = storage.remove(itemId);
+  if (matchedItem){
+    return res.status(200).json(matchedItem);
+  }
 
-    return res.status('404').json({error: 'Not Found'});
+  return res.status('404').json({error: 'Not Found'});
 });
 
 
 app.put('/items/:id', jsonParser, function(req, res) {
-    var itemId = parseInt(req.params.id);
-    if(isNaN(itemId)) {
-        return res.status(400).json({error: 'Invalid item id'});
-    }
-    var name = req.body && req.body.name;
-    if(typeof name !== "string" || name.trim() === '') {
-        return res.status(400).json({error: 'Invalid item name'});
-    }
+  var itemId = parseInt(req.params.id);
+  if(isNaN(itemId)) {
+    return res.status(400).json({error: 'Invalid item id'});
+  }
+  var name = req.body && req.body.name;
+  if(typeof name !== "string" || name.trim() === '') {
+    return res.status(400).json({error: 'Invalid item name'});
+  }
 
-    for(var x = 0; x < storage.items.length; x++) {
-        var currentItem = storage.items[x];
-        if(currentItem.id === itemId) {
-            currentItem.name = name;
-            return res.status(200).json(currentItem);
-        }
+  for(var x = 0; x < storage.items.length; x++) {
+    var currentItem = storage.items[x];
+    if(currentItem.id === itemId) {
+      currentItem.name = name;
+      return res.status(200).json(currentItem);
     }
+  }
 
     //max sure there are no future collisions
-    if(storage.id <= itemId) {
-        storage.id = itemId + 1;
-    }
-    var item = {name: name, id: itemId};
-    storage.items.push(item);
-    return res.status(201).json(item);
+  if(storage.id <= itemId) {
+    storage.id = itemId + 1;
+  }
+  var item = {name: name, id: itemId};
+  storage.items.push(item);
+  return res.status(201).json(item);
 });
 
 module.exports = app;
--- test/server-spec.js
+++ test/server-spec.js
@@ -2,23 +2,41 @@ var request = require("supertest");
 var server = require("../server.js");
 var tape = require("tape");
 
+// should list items on get
 tape.test("GET: items", function(t){
   request(server)
   .get("/items")
   .expect(200)
+  .expect('Content-Type', /json/)
   .expect(function(res){
+    t.equals(typeof res.body, 'object');
     t.equals(res.body.length, 3);
+    t.equals(typeof res.body[0], 'object');
+    t.not(res.body[0].id, undefined);
+    t.not(res.body[0].name, undefined);
+    t.equals(typeof res.body[0].id, 'number');
+    t.equals(typeof res.body[0].name, 'string');
+    t.equals(res.body[0].name, 'Broad beans');
+    t.equals(res.body[1].name, 'Tomatoes');
+    t.equals(res.body[2].name, 'Peppers');
   }).end(t.end); //<-- We have to call SuperTest.end() to shut down the server,
   // We pass it t.end() so that tape knows we're done with this async test
 });
 
+// should add an item on post
 tape.test("POST: items", function(t){
   request(server)
   .post("/items")
   .send({name:"Toast"})
   .expect(201)
+  .expect('Content-Type', /json/)
   .expect(function(res){
-    t.deepEqual(res.body.name, "Toast");
+    t.equals(typeof res.body, 'object');
+    t.not(res.body.id, undefined);
+    t.not(res.body.name, undefined);
+    t.equals(typeof res.body.id, 'number');
+    t.equals(typeof res.body.name, 'string');
+    t.equals(res.body.name, "Toast");
   }).end(t.end);
 });
 
@@ -27,7 +45,73 @@ tape.test("POST: items - empty body", function(t){
   .post("/items")
   .send(undefined)
   .expect(400)
+  .expect('Content-Type', /json/)
   .expect(function(res){
     t.deepEqual(res.body.error, "Invalid item body");
+  }).end(t.end);
+});
+
+// should edit an item on put
+tape.test("PUT: items", function(t){
+  request(server)
+  .put("/items/1")
+  .send({name: "Milk"})
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .expect(function(res){
+    t.equals(typeof res.body, 'object');
+    t.not(res.body.id, undefined);
+    t.not(res.body.name, undefined);
+    t.equals(typeof res.body.id, 'number');
+    t.equals(typeof res.body.name, 'string');
+    t.equals(res.body.name, "Milk");
+  }).end(t.end);
+});
+
+// create an item on put if id not exists
+tape.test("PUT: items - non existing item", function(t){
+  request(server)
+  .put("/items/7")
+  .send({name: "New Milk"})
+  .expect(201)
+  .expect('Content-Type', /json/)
+  .expect(function(res){
+    t.equals(typeof res.body, 'object');
+    t.not(res.body.id, undefined);
+    t.not(res.body.name, undefined);
+    t.equals(typeof res.body.id, 'number');
+    t.equals(typeof res.body.name, 'string');
+    t.equals(res.body.id, 7);
+    t.equals(res.body.name, "New Milk");
+  }).end(t.end);
+});
+
+// TODO:  the ordering of these tests bother me.
+// Is it dependent on the test above finishing in time for the item to exist?
+//  it('should delete an item on delete');
+tape.test("DELETE: items", function(t){
+  request(server)
+  .delete("/items/7")
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .expect(function(res){
+    t.equals(typeof res.body, 'object');
+    t.not(res.body.id, undefined);
+    t.not(res.body.name, undefined);
+    t.equals(typeof res.body.id, 'number');
+    t.equals(typeof res.body.name, 'string');
+    t.equals(res.body.id, 7);
+    t.equals(res.body.name, "New Milk");
+  }).end(t.end);
+});
+
+//  non existing item
+tape.test("DELETE: items - non existing item", function(t){
+  request(server)
+  .delete("/items/9")
+  .expect(404)
+  .expect('Content-Type', /json/)
+  .expect(function(res){
+    t.deepEqual(res.body.error, "Not Found");
   }).end(t.end);
 });
